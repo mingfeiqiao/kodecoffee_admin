80f721fe8f56c4ab83cd4e1adc85bc100916119c
yuanshaokai <yuanshaokai@zingfront.com>
2023-12-14 15:41:54 +0800

    恢复请求接口地址

--- src/page/main-menu.vue
+++ src/page/main-menu.vue
@@ -143,8 +143,8 @@ export default {
         }
       }
     }
-    // this.loginOrRegisterUser()
-    this.test();
+    this.loginOrRegisterUser()
+    // this.test();
     this.initOptions();
     this.currentMenu = this.$route.path; // 初始化当前路由路径
   },
